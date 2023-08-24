b72945fa3c7a4209c2e4c98e1300b4b1e642744e
StarkLSH <seungheon1029@gmail.com>
2023-08-24 18:20:02 +0900

    feat: navbar 컴포넌트 개발

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
